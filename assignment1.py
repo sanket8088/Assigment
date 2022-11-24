de48006d48f1c09c81c73615f43dc26404838350
Sanket <32214164+sanket8088@users.noreply.github.com>
2022-11-24 21:58:02 +0530

    Create assignment1.py

---
+++ assignment1.py
@@ -0,0 +1,52 @@
+# 1
+# Given a list, write a Python program to swap first and last element of the list.
+# Examples: 
+# Input : [12, 35, 9, 56, 24]
+# Output : [24, 35, 9, 56, 12]
+
+# Input : [1, 2, 3]
+# Output : [3, 2, 1]
+
+
+# 2
+# Given a list in Python and provided the positions of the elements, write a program to swap the two elements in the list.
+
+# Examples:  
+# Input : List = [23, 65, 19, 90], pos1 = 1, pos2 = 3
+# Output : [19, 65, 23, 90]
+
+# Input : List = [1, 2, 3, 4, 5], pos1 = 2, pos2 = 5
+# Output : [1, 5, 3, 4, 2]
+
+
+#3
+# Python | Find the length of a string wihtout using len method
+
+
+#4
+#Define a function which takes dictionary as an argument and print sorted dictionary by key first
+# and then a value
+
+# Example
+# {"python" : 5, "javascript" : 10, "c#": 9, "golang" : 3, "java" : 1}
+# First print statement should be sorted based on key (alphabetical)
+# Second print statement should be sorted based on values (numerical)
+
+
+
+#5
+
+# Leetcode platform
+"""
+1. https://leetcode.com/problems/longest-substring-without-repeating-characters/
+2. https://leetcode.com/problems/integer-to-roman/
+3. https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+
+"""
+
+
+#6 Define a function to convert number to words for upto four digit. Note that you can use any package or library.
+# Every word should be defined by you
+# Example -- int2_word(5) --> five
+# int2_word(999) ---> Nine hundred ninety nine
+# int2_word(9992) ---> nine thousand nine hundred ninety two
